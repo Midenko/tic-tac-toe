53051c87f0249e817db11e1091bd623314488513
Midenko <kostyagrand@mail.ru>
2017-02-03 22:34:33 +0400

    Add files via upload

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,16 @@
 class TicTacToe {
     constructor() {
-
-    }
+        this.count = 0; 
+        this.field = [ [null, null, null], [null, null, null], [null, null, null] ];
+        }
 
     getCurrentPlayerSymbol() {
-
+        if (this.count % 2 == 0) return 'x';
+        else return 'o';
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        this.field[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
     }
 
     isFinished() {
@@ -28,7 +30,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
